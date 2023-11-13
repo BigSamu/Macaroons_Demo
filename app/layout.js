192661809ef71c84521997498eda07541620fbc7
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-13 17:49:29 +0000

    updating SEO image v2

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Head from "next/head"; // Import the Head component
 
 import Layout from "../components/Layout";
 import { AuthProvider } from "../contexts/AuthContext";
@@ -10,7 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Macaroons Demo",
-  description: "A demo of macaroon tokens using First-Party Caveats built with NextJS and FastAPI", // You can update this as needed
+  description:
+    "A demo of macaroon tokens using First-Party Caveats built with NextJS and FastAPI", // You can update this as needed
   icons: {
     icon: "/assets/favicon.ico",
   },
@@ -20,19 +20,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href={metadata.icons.icon} />
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+      <link rel="icon" href={metadata.icons.icon} />
 
-        {/* Add other SEO relevant tags here */}
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://macaroons-demo-starter.vercel.app" />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.icons.icon} />
-      </Head>
+      {/* Add other SEO relevant tags here */}
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://macaroons-demo-starter.vercel.app"
+      />
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="og:image" content={metadata.image} />
+
       <body className={inter.className}>
         <AuthProvider>
           <ResourceProvider>
