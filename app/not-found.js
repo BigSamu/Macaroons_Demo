be03d1a3ce13a14b5f7484dafcb84315363dc9c8
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-11-14 16:14:02 +0000

    adding page wrapper for not found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+import NotFoundPageWrapper from "../components/PageWrappers/NotFoundPageWrapper";
 
 //******************************************************************************
 // MAIN COMPONENT
@@ -11,23 +11,9 @@ const NotFound = () => {
   //------------------------------------------
 
   return (
-    <div className="text-center my-5">
-      <h1>Ooooops...</h1>
-      <h2>That page you are looking cannot be found</h2>
-      <div className="d-flex flex-column justify-content-center align-items-center h-100">
-        <img
-          src={"/assets/crying_macaroon_2.jpeg"}
-          className="img-fluid"
-          width={360}
-        ></img>
-      </div>
-      <h5 className="mt-3">
-        Go back to{" "}
-        <span className="button btn-link">
-          <Link href="/">Homepage</Link>
-        </span>
-      </h5>
-    </div>
+    <>
+      <NotFoundPageWrapper />
+    </>
   );
 };
 
